7c8681910210ef5b315f39cf129de1223366357b
Joe Case <jcase146@gmail.com>
2015-12-30 08:32:50 -0500

    Fix solutions. Fix filename on add-it-up/my_solution.rb

---
+++ week-4/add-it-up/my_solution.rb
@@ -0,0 +1,60 @@
+# Add it up!
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# I worked on this challenge with Ryan Salerno.
+
+# 0. total Pseudocode
+# make sure all pseudocode is commented out!
+
+# Input:
+# Output:
+# Steps to solve the problem.
+
+
+# 1. total initial solution
+
+def total(array)
+ sum = 0
+ counter = 0
+ while counter < array.length
+   sum = sum + array[counter]
+   counter += 1
+ end
+ return sum
+end
+
+# 3. total refactored solution
+
+
+
+# 4. sentence_maker pseudocode
+# make sure all pseudocode is commented out!
+# Input: An Array of strings
+# Output: A unified string
+# Steps to solve the problem.
+
+# create holding variable
+# loop through array pushing each element to holding variable
+# return complete holding variable
+
+# 5. sentence_maker initial solution
+# the_array = ["all", "my", "socks", "are", "dirty"]
+
+def sentence_maker(array)
+  full_string = array[0]
+  counter = 1
+  while counter < array.length
+    full_string = full_string + " " + array[counter].to_s
+    counter += 1
+  end
+  full_string.capitalize + "."
+end
+
+# sentance_maker(the_array)
+
+
+# 6. sentence_maker refactored solution
+
--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -5,5 +5,19 @@
 
 # Your Solution Below
 def factorial(number)
-  # Your code goes here
+  return 1 if number == 0
+  # Create an array with descending numbers
+  num_array = []
+  while number > 0
+    num_array.push(number)
+    number = number - 1
+  end
+  # Multiply numbers is array
+  answer = num_array[0]
+  counter = 1
+  while counter < num_array.length
+    answer = answer * num_array[counter]
+    counter = counter  + 1
+  end
+  answer
 end
