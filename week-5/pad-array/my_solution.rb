89e5b07c72c9d56e066659e3c4c151a2e69e487b
Joe Case <jcase146@gmail.com>
2016-01-10 12:31:13 -0500

    Final full solution

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -1,6 +1,6 @@
 # Pad an Array
 
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge with Jeremy Oltean
 
 # I spent [] hours on this challenge.
 
@@ -13,19 +13,50 @@
 
 # 0. Pseudocode
 
-# What is the input?
+# What is the input? : an array, a length and a value
 # What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+# Out is the modified array
+# # What are the steps needed to solve the problem?
+#   evaluate array for length
+#   Then add cells if empty at index with value if less than length.
 
+array_test = [1,2,3]
 
 # 1. Initial Solution
 def pad!(array, min_size, value = nil) #destructive
-  # Your code here
+  array if (array.length <= min_size) || (min_size == 0)
+  # return array if min_size == 0
+  counter = array.length
+  while counter < min_size
+    array << value
+    counter += 1
+  end
+  array
 end
 
+print pad!(array_test, 0, 9)
+print array_test
+
+
+array_test2 = [1,2,3,4,5]
+
 def pad(array, min_size, value = nil) #non-destructive
-  # Your code here
+  new_array = []
+  new_array = array.clone if min_size == 0
+  counter = 0
+  while counter < min_size
+    if array[counter] != nil
+      new_array << array[counter]
+    else
+      new_array << value
+    end
+    counter += 1
+  end
+  new_array
 end
+
+print pad(array_test2, 8, 6)
+print array_test2
 
 
 # 3. Refactored Solution
