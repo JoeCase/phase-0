daa74880f4e6d548c22f42437b4884a7cf9b4ad8
Joe Case <jcase146@gmail.com>
2016-01-19 13:57:36 -0500

    Start 7.8

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,3 @@
+// As I user, I want to find the sum of all the numbers in an array.
+// As I user, I want to find the average or mean of all the numbers in an array.
+// As I user, I want to find the median value of all the numbers in an array.
